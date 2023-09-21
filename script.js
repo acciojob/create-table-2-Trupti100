8b9165e71ba5d414f7fbc0fe4217e3a0b9dd648b
Trupti100 <127935389+Trupti100@users.noreply.github.com>
2023-09-21 12:03:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+// Get references to the button and the table
+const createTableBtn = document.getElementById('createTableBtn');
+const myTable = document.getElementById('myTable');
+
+// Function to create the table
 function createTable() {
-    //Write your code here
-  
+    // Prompt the user to enter the number of rows
+    const numRows = parseInt(prompt('Input number of rows'));
+
+    if (!isNaN(numRows) && numRows >= 0) {
+        // Clear the existing table if any
+        myTable.innerHTML = '';
+
+        // Create the table rows and cells
+        for (let i = 0; i < numRows; i++) {
+            const row = myTable.insertRow(i);
+            for (let j = 0; j < numRows; j++) {
+                const cell = row.insertCell(j);
+                cell.textContent = `Row-${i} Column-${j}`;
+            }
+        }
+    } else {
+        alert('Please enter a valid number of rows.');
+    }
 }
+
+// Add a click event listener to the button
+createTableBtn.addEventListener('click', createTable);
